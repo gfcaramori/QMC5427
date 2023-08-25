70826a3472705193958bf4761af3d2455a242f69
Giovanni F. Caramori <giovanni.caramori@ufsc.br>
2023-08-25 13:55:33 -0300

    Update roots.py

--- chapter-1/roots.py
+++ chapter-1/roots.py
@@ -1,1 +1,20 @@
-
+import math
+print("entre com os valores dos coeficientes a, b, e c: ")
+a = float(input("digite a: "))
+b = float(input("digite b: "))
+c = float(input("digite c: "))
+delta = (b**2) - (4*a*c)
+if delta  < 0:
+    print("A equação não possui raízes reais")
+    raizDelta = math.sqrt(abs(delta))*(0 + 1j)
+    x_1 = (-b + raizDelta)/(2*a)
+    x_2 = (-b - raizDelta)/(2*a)
+    print("As raízes complexas são x_1 = {0} e x_2 = {1}" .format(x_1, x_2))
+elif delta == 0 :
+    x = (-b)/(2*a)
+    print ("A equaçao possui apenas uma raíz x = {0}" .format(x))
+elif delta > 0 :
+    raizDelta = math.sqrt(delta)
+    x_1 = (-b + raizDelta)/(2*a)
+    x_2 = (-b - raizDelta)/(2*a)
+    print("As raízes obtidas são x_1 = {0} e x_2 = {1}" .format(x_1, x_2))
