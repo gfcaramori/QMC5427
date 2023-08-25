23d610a4b7a3feeea4507fbbbd4f04eb39a03482
Giovanni F. Caramori <giovanni.caramori@ufsc.br>
2023-08-25 13:56:53 -0300

    Update total_seconds.py

--- chapter-1/total_seconds.py
+++ chapter-1/total_seconds.py
@@ -1,1 +1,18 @@
-
+s_t = int(input("digite o total de segundos: "))
+s = 1
+h = 3600 * s
+m = 60 * s
+if s_t < 60:
+    print(f'{h * 0}h {m * 0}min {s_t}s')
+elif s_t == 60:
+    m = s_t//60
+    print(f'{h * 0}h {m}min {s_t*0}s')
+elif 60 < s_t < 3600:
+    m = s_t//60
+    s_r = s_t % 60
+    print(f'{h * 0}h {m}min {s_r}s')
+elif s_t >= 3600:
+    h = s_t//3600
+    m_r = (s_t % 3600)//60
+    s_r = (s_t % 3600) % 60
+    print(f'{h}h {m_r}min {s_r}s')
